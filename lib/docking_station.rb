8d41dfef3b5e5d46f8b316ffd05983934b1dd414
joecoker <joe_coker@hotmail.co.uk>
2018-11-01 17:05:04 +0000

    Added private methods and error tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,30 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike, :bike_rack
+
+  def initialize
+    @bike_rack = []
+  end
 
   def release_bike
-    fail "No bikes left" unless bike 
-    Bike.new
+    fail "No bikes left" if empty?
+    bike_rack.pop
   end
 
   def dock(bike)
-   @bike = bike
+   fail "Docking station full" if full?
+   bike_rack.push(bike)
   end
 
-  def bike
-    @bike
+private
+
+  def full?
+    true if bike_rack.length == 20
   end
+
+  def empty?
+    true if bike_rack.length == 0
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,13 +14,18 @@ describe DockingStation do
   end
   it "dock stores a bike" do
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bike_rack).to include(bike)
   end
   it "check whether bike method exists" do
     expect(subject).to respond_to(:bike)
   end
 
   it "#release_bike throw error if no bikes docked" do
-    expect { subject.release_bike } .to raise_error("No bikes left")
+
+    expect { subject.release_bike && (subject.counter.to equal(0))}.to raise_error("No bikes left")
+  end
+
+  it "throw error if #dock when 20 bikes are docked" do
+    expect { 21.times{ subject.dock(bike)} } .to raise_error("Docking station full")
   end
 end
