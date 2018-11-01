9fb5508ae9a9efb3a1b1490a9b238e33d2ac4996
joecoker <joe_coker@hotmail.co.uk>
2018-11-01 14:53:33 +0000

    Added tests to catch errors and refactor

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike 
+  attr_reader :bike
+
   def release_bike
+    fail "No bikes left" unless bike 
     Bike.new
   end
+
   def dock(bike)
    @bike = bike
   end
+
   def bike
     @bike
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,26 @@
 require 'docking_station.rb'
 
+
 describe DockingStation do
+  let (:docking_station) {DockingStation.new}
+  let (:bike) {Bike.new}
+
   it {is_expected.to respond_to :release_bike}
 
-  it "#release_bike will get a new instance of bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it "#release_bike will get a docked bike" do
+    subject.dock(bike)
+    tricycle = subject.release_bike
+    expect(tricycle).to be_working
   end
   it "dock stores a bike" do
-    bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike 
+    expect(subject.bike).to eq bike
   end
   it "check whether bike method exists" do
     expect(subject).to respond_to(:bike)
   end
 
+  it "#release_bike throw error if no bikes docked" do
+    expect { subject.release_bike } .to raise_error("No bikes left")
+  end
 end
